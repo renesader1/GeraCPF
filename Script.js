d5874f6aa6f598146426df6a40edfa48095fb05e
renesader1 <69492702+renesader1@users.noreply.github.com>
2023-05-10 00:32:27 -0300

    update

--- Script.js
+++ Script.js
@@ -1,15 +1,25 @@
 listaInputs = document.querySelectorAll(".input-CPF")
 botaoEnviar = document.querySelector("#envia-CPF-Botão")
 
-
 listaInputs.forEach(element => {
     element.addEventListener("keyup", (e) =>{
 
-        if(e.key == "Backspace"){
+        if(e.key == "Backspace" && e.target.previousElementSibling != null){
             e.target.previousElementSibling.focus()
         }
-        else{
+        else if(e.key != "Backspace" && e.target.nextElementSibling != null){
             e.target.nextElementSibling.focus()
         }
     })
 });
+
+botaoEnviar.addEventListener("click", (e) => {
+    e.preventDefault()
+    geraCPF()
+})
+
+function geraCPF(){
+    listaInputs.forEach(element => {
+        console.log(element.value)
+    })
+}
